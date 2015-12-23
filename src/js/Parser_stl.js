90f576d25dba851608535709e0d9f0aa696a02f8
Max Petruck <petruck.maxim@gmail.com>
2015-12-23 17:11:17 +0200

    Update parsing functions
    - functions return object as result
    Result format:
    {
      vertices - Float32Array
      normals - Float32Array
    }
    - add normals duplication

--- src/js/Parser_stl.js
+++ src/js/Parser_stl.js
@@ -1,49 +1,61 @@
 var parseStlBinary = function(stl) {
-  var dv = new DataView(stl, 80), // 80 bytes unused header
+  var data = new DataView(stl, 80), // 80 bytes unused header
       isLittleEndian = true, // All binary STLs are assumed to be little endian
-      triangles = dv.getUint32(0, isLittleEndian),
-      normals = new Float32Array(triangles*3),
+      triangles = data.getUint32(0, isLittleEndian),
+      normals = new Float32Array(triangles*9),
       vertices = new Float32Array(triangles*9),
       i,
       j,
       k,
-      offset = 4;
+      offset = 4,
+      normal = [];
 
   for (i = 0; i < triangles; i+=3) {
 
     // Get the normal for this triangle
     // I didn't save it cause I'll compute normals for vertices later
-    normals[i] = dv.getFloat32(offset, isLittleEndian);
-    normals[i+1] = dv.getFloat32(offset+4, isLittleEndian);
-    normals[i+2] = dv.getFloat32(offset+8, isLittleEndian);
+    normal[0] = data.getFloat32(offset, isLittleEndian);
+    normal[1] = data.getFloat32(offset+4, isLittleEndian);
+    normal[2] = data.getFloat32(offset+8, isLittleEndian);
 
     offset += 12;
 
     // Get 3 vertices for triangle
     for (j = 0; j < 3; j++) {
       k = (i+j)*3;
-      vertices[k] = dv.getFloat32(offset, isLittleEndian);
-      vertices[k+1] = dv.getFloat32(offset+4, isLittleEndian);
-      vertices[k+2] = dv.getFloat32(offset+8, isLittleEndian);
+
+      normals[k] = data.getFloat32(offset, isLittleEndian);
+      normals[k+1] = data.getFloat32(offset+4, isLittleEndian);
+      normals[k+2] = data.getFloat32(offset+8, isLittleEndian);
+
+      vertices[k] = data.getFloat32(offset, isLittleEndian);
+      vertices[k+1] = data.getFloat32(offset+4, isLittleEndian);
+      vertices[k+2] = data.getFloat32(offset+8, isLittleEndian);
       offset += 12;
     }
 
     // Skip Uint16 "attribute byte count"
     offset += 2;
   }
+  return {
+    vertices: vertices,
+    normals: normals
+  }
 };
 
-var parseStl = function(stl) {
+var parseStlASCII = function(stl) {
   var state = '',
       lines = stl.split('\n'),
       name,
       parts,
       line,
-      normals,
       done,
-      vertices = [],
       len,
-      i;
+      i,
+      normal = [], // tmp array to hold normal coordinates
+      normals = [],
+      vertices = [];
+
 
   for (len = lines.length, i = 0; i < len; i++) {
     if (done) {
@@ -68,9 +80,9 @@ var parseStl = function(stl) {
           console.error('Invalid state "' + parts[0] + '", should be "facet normal"');
           return;
         } else {
-          normals.push(parseFloat(parts[2]));
-          normals.push(parseFloat(parts[3]));
-          normals.push(parseFloat(parts[4]));
+          normal[0] = parseFloat(parts[2]);
+          normal[1] = parseFloat(parts[3]);
+          normal[2] = parseFloat(parts[4]);
           state = 'facet normal';
         }
         break;
@@ -85,6 +97,11 @@ var parseStl = function(stl) {
         break;
       case 'vertex':
         if (parts[0] === 'vertex') {
+          // Add normal for vertice
+          normals.push(normal[0]);
+          normals.push(normal[1]);
+          normals.push(normal[2]);
+          // Add vertice
           vertices.push(parseFloat(parts[1]));
           vertices.push(parseFloat(parts[2]));
           vertices.push(parseFloat(parts[3]));
@@ -109,9 +126,14 @@ var parseStl = function(stl) {
         if (parts[0] === 'endsolid') {
           done = true;
         } else if (parts[0] === 'facet' && parts[1] === 'normal') {
-          normals.push(parseFloat(parts[2]));
-          normals.push(parseFloat(parts[3]));
-          normals.push(parseFloat(parts[4]));
+          // Add normal for vertice
+          normals.push(normal[0]);
+          normals.push(normal[1]);
+          normals.push(normal[2]);
+          // Add vertice
+          vertices.push(parseFloat(parts[1]));
+          vertices.push(parseFloat(parts[2]));
+          vertices.push(parseFloat(parts[3]));
           state = 'facet normal';
         } else {
           console.error(line);
@@ -126,4 +148,8 @@ var parseStl = function(stl) {
   }
   vertices = new Float32Array(vertices);
   normals = new Float32Array(normals);
+  return {
+    vertices: vertices,
+    normals: normals
+  };
 };
